dd489354edef4e6bb85e5ff59ec39f8ab0c07cd1
oscar agbasi <33052726+oscarAgbasi@users.noreply.github.com>
2018-12-17 02:11:40 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace lab5a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Colour";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Paint += new System.Windows.Forms.PaintEventHandler(this.button2_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(24, 72);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(48, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 73);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(335, 431);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "A drop in a Bucket";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿/*
+  Program: Sample.cs
+  Author:  agbasi oscar
+  Date:    december 7th, 2018
+ 
+  Purpose: This program demonstrates some of the common capabilities of the Graphics object
+           as well as how to properly use them.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5a
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private Pen P;
+        private Pen P1;
+        private SolidBrush b;
+        int w;
+        int h;
+        private Color c;
+        private Color c1 = Color.White;
+        private int _X;
+        private int _Y;
+        private int rate; //value of the track bar
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(button2_Paint);
+            //_X = 355;
+            //_Y = 15;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Show the color dialog.
+            colorDialog1.Color = c;                //Display with the previous colour already chosen
+            DialogResult result = colorDialog1.ShowDialog(); 
+            c = colorDialog1.Color;                //Save the colour choice the user made
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();// closing the appliaction
+        }
+
+        private void button2_Paint(object sender, PaintEventArgs e)
+        {
+            int w = this.ClientSize.Width; // use to get the width of the working area
+            int h = this.ClientSize.Height; // use to get the height of the working area
+            g = e.Graphics; // Get the Graphics object from the PaintEventArgs
+            P = new Pen(c);  //Create a new Pen using the current colour
+            P1 = new Pen(c1);  //Create a new Pen using the current colour
+            b = new SolidBrush(c);  //Create a new brush using the current colour
+
+
+            //g = this.CreateGraphics();    //Create a graphics object
+            //g.DrawRectangle(P1, 100, 220, 200, 150);
+
+
+            g.DrawLine(P1, 90, 370, 270, 370);  //drawing the bucket when the form starts
+            g.DrawLine(P1, 90, 370, 90, 270);
+            g.DrawLine(P1, 270, 370, 270, 270);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //timer1.Start();
+            //g = this.CreateGraphics();
+            //g.FillRectangle(b, 118, 200, 15, 170);
+
+           
+        }
+
+        int i = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++; //increment the counter
+            g = this.CreateGraphics();
+
+            P.Color = c;
+            g.DrawLine(P, 91, 370 - i, 269, 370 - i); //these lines fill up the bucket
+
+            if (i == 100) //when the bucket fills up
+            {
+                timer1.Stop(); //stop the timer
+                trackBar1.Value = 0; //stops the tap
+                i = 0; //sets counter to 0
+            }
+
+            //g.FillRectangle(b, 100, _X, 200, _Y);//strating bucket fills
+            //_X -= 15;
+            //_Y += 2;
+
+            ////g.FillRectangle(b, 100, 355, 200, 150) button liqud;  g.FillRectangle(b, 100, 320, 200, 20);
+            ////g.FillRectangle(b, 100, 220, 200, 150);
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+
+            rate = trackBar1.Value; //
+
+            if (rate > 0)
+            {
+                b = new SolidBrush(c);
+                g.FillRectangle(b, 110, 180, 25, 190 - i);
+                timer1.Interval = 250 / rate;
+                timer1.Start();
+            }
+            if (rate == 0)
+            {
+                b.Color = Color.Black;
+                g.FillRectangle(b, 110, 180, 25, 190 - i);
+                timer1.Stop();
+            }
+        }
+    }
+    }
+
